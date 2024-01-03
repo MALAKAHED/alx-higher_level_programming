bdd8e9d3630bfb2af55821e9dc5396c27aeddfbb
MALAKAHED <lokamalek776@gmail.com>
2024-01-03 10:16:04 -0800

    tasks

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,21 +1,46 @@
 #include "lists.h"
-/**
-* check_cycle - checks if a linked list contains a cycle
-* @list: linked list to check
-* Return: 1 if the list has a cycle, 0 if it doesn't
-*/
-int check_cycle(listint_t *list)
 
+size_t print_listint(const listint_t *h)
+{
+size_t nodes = 0;
+while (h)
+{
+printf("%d\n", h->n);
+h = h->next;
+nodes++;
+}
+return nodes;
+}
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+listint_t *new_node = malloc(sizeof(listint_t));
+if (!new_node)
+return NULL;
+new_node->n = n;
+new_node->next = *head;
+*head = new_node;
+return new_node;
+}
+void free_listint(listint_t *head)
+{
+listint_t *temp;
+while (head)
+{
+temp = head;
+head = head->next;
+free(temp);
+}
+}
+int check_cycle(listint_t *list)
+{
 listint_t *slow = list;
 listint_t *fast = list;
-if (!list)
-return (0);
-while (slow && fast && fast->next)
+while (fast && fast->next)
 {
 slow = slow->next;
 fast = fast->next->next;
 if (slow == fast)
-return (1);
+return 1;
 }
-return (0);
+return 0;
 }
