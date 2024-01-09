2ff8c3e72584168f16a3eeae24e0d93693508063
MALAKAHED <lokamalek776@gmail.com>
2024-01-09 10:50:29 -0800

    commit

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-for i in range(10):
-for j in range(i + 1, 10):
-print("{:d}{:d}".format(i, j), end=", " 
-f i != 8 or j != 9 else "\n")
+for i in range(0, 99):
+    print("{} = {}".format(i, hex(i)))
